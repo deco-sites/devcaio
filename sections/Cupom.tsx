c603ceabf08856416f884126b6548e9552af180f
Vieira <caio2011dhd@gmail.com>
2024-04-21 14:22:05 -0300

    nome

--- sections/Cupom.tsx
+++ sections/Cupom.tsx
@@ -3,7 +3,7 @@ export interface Props {
   description: string;
 }
 
-export default function Cupom ({ code, description }: Props) {
+export default function Cupom({ code, description }: Props) {
   return (
     <div class="container mb-2 bg-gradient-to-r  text-center from-indigo-500 to-violet-500 text-white p-8 rounded-lg shadow-lg max-w-md mx-auto">
       <p class="block font-bold text-lg">{code}</p>
--- sections/Temperature.tsx
+++ sections/Temperature.tsx
@@ -4,7 +4,7 @@ type Props = {
   celcius: string;
 };
 
-export default function Temperature ({ celcius }: Props) {
+export default function Temperature({ celcius }: Props) {
   return (
     <div class="container mb-2 bg-gradient-to-r  text-center from-indigo-500 to-violet-500 text-white p-8 rounded-lg shadow-lg max-w-md mx-auto">
       <p class="block font-bold text-lg"></p>
