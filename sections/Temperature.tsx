6379062b9c88c95176d45b5a34c248b298688f44
Vieira <caio2011dhd@gmail.com>
2024-04-21 14:06:45 -0300

    manigest

---
+++ sections/Temperature.tsx
@@ -0,0 +1,15 @@
+import { Temperature } from "apps/weather/loaders/temperature.ts";
+
+type Props = {
+  celcius: Temperature;
+};
+
+export default function ({ celcius }: Props) {
+  return (
+    <div class="container mb-2 bg-gradient-to-r  text-center from-indigo-500 to-violet-500 text-white p-8 rounded-lg shadow-lg max-w-md mx-auto">
+      <p class="block font-bold text-lg">}</p>
+      <br />
+      {celcius}
+    </div>
+  );
+}
