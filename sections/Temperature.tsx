809576947a825b17449888eb16390dcecc7d8413
Vieira <caio2011dhd@gmail.com>
2024-04-21 14:20:44 -0300

    temperature

--- sections/Temperature.tsx
+++ sections/Temperature.tsx
@@ -4,10 +4,10 @@ type Props = {
   celcius: string;
 };
 
-export default function ({ celcius }: Props) {
+export default function Temperature ({ celcius }: Props) {
   return (
     <div class="container mb-2 bg-gradient-to-r  text-center from-indigo-500 to-violet-500 text-white p-8 rounded-lg shadow-lg max-w-md mx-auto">
-      <p class="block font-bold text-lg">}</p>
+      <p class="block font-bold text-lg"></p>
       <br />
       {celcius}
     </div>
